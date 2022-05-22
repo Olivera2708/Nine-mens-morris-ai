6eb61b80687223ad1dd999b640fcbfb6c33f3bb3
Olivera2708 <>
2022-05-22 21:07:19 +0200

    added hashmaps

--- hashmap.py
+++ hashmap.py
@@ -1,30 +1,15 @@
-"""
-Modul sadrži implementacije heš mape
-"""
 import random
 
-from map import Map, MapElement
+from map import MapElement
 
 
 class HashMap(object):
-    """
-    Klasa modeluje heš mapu
-    """
-
     def __init__(self, capacity=8):
-        """
-        Konstruktor
-
-        Argumenti:
-        - `capacity`: inicijalni broj mesta u lookup nizu
-        - `prime`: prost broj neophodan heš funkciji
-        """
         self._data = capacity * [None]
         self._capacity = capacity
         self._size = 0
         self.prime = 109345121
 
-        # konstante heširanja
         self._a = 1 + random.randrange(self.prime-1)
         self._b = random.randrange(self.prime)
 
@@ -32,28 +17,11 @@ class HashMap(object):
         return self._size
 
     def _hash(self, x):
-        """
-        Heš funkcija
-
-        Izračunavanje heš koda vrši se po formuli (ax + b) mod p.
-
-        Argument:
-        - `x`: vrednost čiji se kod računa
-        """
         hashed_value = (hash(x)*self._a + self._b) % self.prime
         compressed = hashed_value % self._capacity
         return compressed
 
     def _resize(self, capacity):
-        """
-        Skaliranje broja raspoloživih slotova
-
-        Metoda kreira niz sa unapred zadatim kapacitetom u koji
-        se prepisuju vrednosti koje se trenutno nalaze u tabeli.
-
-        Argument:
-        - `capacity`: kapacitet novog niza
-        """
         old_data = list(self.items())
         self._data = capacity * [None]
         self._size = 0
@@ -63,16 +31,6 @@ class HashMap(object):
             self[k] = v
 
     def __getitem__(self, key):
-        """
-        Pristup elementu sa zadatim ključem
-
-        Apstraktna metoda koja opisuje pristup elementu na osnovu
-        njegovog ključa. Implementacija pristupa bucketu varira u
-        zavisnosti od načina rešavanja kolizija.
-
-        Argument:
-        - `key`: ključ elementa kome se pristupa
-        """
         bucket_index = self._hash(key)
         return self._bucket_getitem(bucket_index, key)
 
@@ -104,25 +62,13 @@ class HashMap(object):
 
 
 class LinearHashMap(HashMap):
-    """
-    Klasa modeluje heš mapu koja rešava kolizije linearnim pretraživanjem
-    """
     # sentinel koji označava lokaciju u mapi sa koje je obrisan element
     _MARKER = object()
 
     def _is_available(self, bucket_index):
-        """
-        Metoda proverava da li je bucket sa zadatim indeksom slobodan
-
-        Argument:
-        - `i`: indeks bucketa
-        """
         return self._data[bucket_index] is None or self._data[bucket_index] is self._MARKER
 
     def _find_bucket(self, bucket_index, key):
-        """
-        Metoda pronalazi bucket koji sadrži element sa zadatim ključem
-        """
         available_slot = None
         while True:
             if self._is_available(bucket_index):
@@ -138,36 +84,12 @@ class LinearHashMap(HashMap):
             bucket_index = (bucket_index+1) % len(self._data)
 
     def _bucket_getitem(self, bucket_index, key):
-        """
-        Pristup elementu u okviru bucketa
-
-        Metoda najpre pristupa bucketu sa zadatim indeksom. Ukoliko
-        bucket postoji, pretražuje se. Ako je element pronađen metoda
-        vraća njegovu vrednostu, dok se u suprotnom podiže odgovarajući
-        izuzetak.
-
-        Argumenti:
-        - `i`: indeks bucketa
-        - `key`: ključ elementa
-        """
         found, index = self._find_bucket(bucket_index, key)
         if not found:
             raise KeyError('Ne postoji element sa traženim ključem.')
         return self._data[index].value
 
     def _bucket_setitem(self, bucket_index, key, value):
-        """
-        Postavljanje vrednosti elementa sa zadatim ključem
-
-        Metoda najpre pronalazi bucket sa zadatim indeksom, a zatim
-        dodaje novi element ili ažurira postojeći na osnovu zadatog
-        ključa. Ukoliko bucket ne postoji, kreira se novi.
-
-        Argumenti:
-        - `i`: indeks bucketa
-        - `key`: ključ elementa
-        - `value`: vrednost elementa
-        """
         found, available_bucket_index = self._find_bucket(bucket_index, key)
         if not found:
             self._data[available_bucket_index] = MapElement(key, value)
@@ -176,18 +98,6 @@ class LinearHashMap(HashMap):
             self._data[available_bucket_index].value = value
 
     def _bucket_delitem(self, bucket_index, key):
-        """
-        Brisanje elementa sa zadatim ključem
-
-        Metoda najpre pristupa bucketu sa zadatim indeksom. Ukoliko
-        bucket postoji, pretražuje se. Ako je element pronađen vrši
-        se njegovo brisanje, u suprotnom se podiže odgovarajući
-        izuzetak.
-
-        Argumenti:
-        - `i`: indeks bucketa
-        - `key`: ključ elementa
-        """
         found, index = self._find_bucket(bucket_index, key)
         if not found:
             raise KeyError('Ne postoji element sa traženim ključem.')
@@ -206,28 +116,3 @@ class LinearHashMap(HashMap):
         for i in range(total_buckets):
             if not self._is_available(i):
                 yield self._data[i].key, self._data[i].value
-
-
-if __name__ == '__main__':
-    print("\nLinear Hash Map\n---------------------")
-    hash_map = LinearHashMap()
-    hash_map[3] = 10
-    hash_map[2] = 11
-
-    print('Inicijalno dodavanje')
-    for item in hash_map:
-        print(item, hash_map[item])
-
-    print('Izmena vrednosti')
-    hash_map[3] = 5
-    for item in hash_map:
-        print(item, hash_map[item])
-
-    print('Brisanje elementa')
-    del hash_map[2]
-    for item in hash_map:
-        print(item, hash_map[item])
-
-
-
-
