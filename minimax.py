5e87dc9df441f09c116f411a5c19463183f26aa1
Olivera2708 <olivera.radovanovic2002@gmal.com>
2022-06-02 12:52:13 +0200

    final

--- minimax.py
+++ minimax.py
@@ -37,7 +37,6 @@ def sledeci_potez_faza1(a, b, dubina, broj_postavljenih, max_igrac):
                 if rezultat > maksimum:
                     maksimum = rezultat
                     najbolji = (i, 0)
-    print(maksimum)
     return najbolji
 
 def minimax_faza1(a, b, dubina, alfa, beta, max_igrac, broj_postavljenih):
@@ -68,7 +67,7 @@ def minimax_faza1(a, b, dubina, alfa, beta, max_igrac, broj_postavljenih):
                         if beta <= alfa:
                             break
             else:
-                rezultat = minimax_faza1(a, bx, dubina-1, alfa, beta, False, broj_postavljenih+1)
+                rezultat = minimax_faza1(ax, bx, dubina-1, alfa, beta, False, broj_postavljenih+1)
                 maksimum = max(rezultat, maksimum)
                 alfa = max(alfa, rezultat)
                 if beta <= alfa:
@@ -92,7 +91,7 @@ def minimax_faza1(a, b, dubina, alfa, beta, max_igrac, broj_postavljenih):
                         if beta <= alfa:
                             break
             else:
-                rezultat = minimax_faza1(ax, b, dubina-1, alfa, beta, True, broj_postavljenih+1)
+                rezultat = minimax_faza1(ax, bx, dubina-1, alfa, beta, True, broj_postavljenih+1)
                 minimum = min(rezultat, minimum)
                 beta = min(beta, rezultat)
                 if beta <= alfa:
@@ -157,58 +156,54 @@ def minimax_faza2(a, b, dubina, alfa, beta, max_igrac):
     if max_igrac:
         maksimum = float('-inf')
         for i in moja_polja(b):
-            mogucnosti = moguc_potez(a, b, i)
-            if mogucnosti != ():
+            for j in moguc_potez(a, b, i):
                 ax = a
                 bx = b
                 bx = ukloni(bx, i)
-                for j in mogucnosti:
-                    bx = dodaj(bx, j)
-                    #proveri jel mill
-                    if jel_mill(bx, j)[0]:
-                        for j in moja_polja(a):
-                            if moze_da_se_ukloni(a, j):
-                                ax = ukloni(ax, j)
-                                rezultat = minimax_faza2(ax, bx, dubina-1, alfa, beta, False)
-                                rezultat += 70
-                                maksimum = max(rezultat, maksimum)
-                                alfa = max(alfa, rezultat)
-                                if beta <= alfa:
-                                    break
-                    else:
-                        rezultat = minimax_faza2(ax, bx, dubina-1, alfa, beta, False)
-                        maksimum = max(rezultat, maksimum)
-                        alfa = max(alfa, rezultat)
-                        if beta <= alfa:
-                            break
+                bx = dodaj(bx, j)
+                #proveri jel mill
+                if jel_mill(bx, j)[0]:
+                    for j in moja_polja(a):
+                        if moze_da_se_ukloni(a, j):
+                            ax = ukloni(ax, j)
+                            rezultat = minimax_faza2(ax, bx, dubina-1, alfa, beta, False)
+                            rezultat += 70
+                            maksimum = max(rezultat, maksimum)
+                            alfa = max(alfa, rezultat)
+                            if beta <= alfa:
+                                break
+                else:
+                    rezultat = minimax_faza2(ax, bx, dubina-1, alfa, beta, False)
+                    maksimum = max(rezultat, maksimum)
+                    alfa = max(alfa, rezultat)
+                    if beta <= alfa:
+                        break
         return maksimum
     
     else:
         minimum = float('inf')
         for i in moja_polja(a):
-            mogucnosti = moguc_potez(a, b, i)
-            if mogucnosti != ():
+            for j in moguc_potez(a, b, i):
                 ax = a
                 bx = b
                 ax = ukloni(ax, i)
-                for j in mogucnosti:
-                    ax = dodaj(ax, j)
-                    #proveri jel mill
-                    if jel_mill(ax, j)[0]:
-                        for j in moja_polja(b):
-                            if moze_da_se_ukloni(b, j):
-                                bx = ukloni(bx, j)
-                                rezultat = minimax_faza2(ax, bx, dubina-1, alfa, beta, True)
-                                rezultat -= 70
-                                minimum = min(rezultat, minimum)
-                                beta = min(beta, rezultat)
-                                if beta <= alfa:
-                                    break
-                    else:
-                        rezultat = minimax_faza2(ax, bx, dubina-1, alfa, beta, True)
-                        minimum = min(rezultat, minimum)
-                        beta = min(beta, rezultat)
-                        if beta <= alfa:
-                            break
+                ax = dodaj(ax, j)
+                #proveri jel mill
+                if jel_mill(ax, j)[0]:
+                    for j in moja_polja(b):
+                        if moze_da_se_ukloni(b, j):
+                            bx = ukloni(bx, j)
+                            rezultat = minimax_faza2(ax, bx, dubina-1, alfa, beta, True)
+                            rezultat -= 70
+                            minimum = min(rezultat, minimum)
+                            beta = min(beta, rezultat)
+                            if beta <= alfa:
+                                break
+                else:
+                    rezultat = minimax_faza2(ax, bx, dubina-1, alfa, beta, True)
+                    minimum = min(rezultat, minimum)
+                    beta = min(beta, rezultat)
+                    if beta <= alfa:
+                        break
         return minimum
     
